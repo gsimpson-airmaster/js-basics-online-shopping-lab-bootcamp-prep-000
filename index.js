d92201768cd4a2cf77f0633e3dab00b5880ae6bf
Glen Simpson <gsimpson@airmaster.com.au>
2018-07-01 06:23:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,8 +69,8 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if(cardNumber != null) {
+  if(cardNumber !== null) {
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }
-  return `
+  return `Sorry, we don't have a credit card on file for you.`;
 }
