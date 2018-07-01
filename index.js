94de12cdbc79dd5334c5e6278e97bf19a97991a2
Glen Simpson <gsimpson@airmaster.com.au>
2018-07-01 05:50:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,27 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var product = {};
+  product[item] = Math.floor((Math.random() * 100));
+  cart.push(product);
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  s1 = "Your shopping cart is empty";
+  s2 = "In your cart, you have ";
+  if(cart.length > 0){
+    if(cart.length > 1) {
+        
+    }
+    else {
+      s2 += `${cart[0][key]} at $${cart[0][value]}.`;
+    }
+    
+  }
+  else {
+    return s1;
+  }
 }
 
 function total() {
